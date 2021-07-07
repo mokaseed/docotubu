06cdb624adcdc719b17bca0fc64c371da0caaedd
Momoka Oyama <pglmmk@gmail.com>
2021-07-07 16:20:50 +0900

    First commit

---
+++ src/main/java/dao/MutterDAO.java
@@ -0,0 +1,68 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Mutter;
+
+public class MutterDAO {
+//	データベース接続に使用する情報
+	private final String JDBC_URL = "jdbc:h2:tcp://localhost/~/docotubu";
+	private final String DB_USER = "sa";
+	private final String DB_PASS = "";
+	
+	public List<Mutter> findAll(){
+		List<Mutter> mutterList = new ArrayList<>();
+		
+		try(Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
+//			SELECT文の準備
+			String sql = "SELECT ID, NAME, TEXT FROM MUTTER ORDER BY ID DESC";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+//			SELECTを実行
+			ResultSet rs = pStmt.executeQuery();
+			
+//			SELECT文の結果をリストに格納
+			while(rs.next()) {
+				int id = rs.getInt("ID");
+				String userName = rs.getString("NAME");
+				String text = rs.getString("TEXT");
+				Mutter mutter = new Mutter(id, userName, text);
+				mutterList.add(mutter);
+			}
+		} catch (SQLException e) {
+//			e.printStackTrace();
+			System.out.println(e);
+			System.out.println("SELECTにてエラー");
+			return null;
+		}
+		return mutterList;
+	}
+	public boolean create(Mutter mutter) {
+		try(Connection conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS)) {
+//			INSERT文の準備（IDは自動連番のため指定しない）
+			String sql = "INSERT INTO MUTTER (NAME, TEXT) VALUES(?, ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+//			INSERT文中の「？」に使用する値を指定しSQLを完成
+			pStmt.setString(1, mutter.getUserName());
+			pStmt.setString(2, mutter.getText());
+			
+//			INSERT文を実行
+			int result = pStmt.executeUpdate();
+			if(result != 1) {
+				return false;
+			}
+		} catch(SQLException e) {
+//			e.printStackTrace();
+			System.out.println(e);
+			System.out.println("INSERTにてエラー");
+			return false;
+		}
+		return true;
+	}
+}
